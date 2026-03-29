acc2f607aa4bed8ad653bf8fa6798dbe09a45bda
Karanvir Sidhu <69744352+karansidhu-03@users.noreply.github.com>
2026-03-28 21:42:44 -0600

    Delete .cloudflare directory

--- .cloudflare/worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    // This tells Cloudflare: "Don't say Hello World, 
-    // just show the files from the 'dist' folder."
-    return env.ASSETS.fetch(request);
-  },
-};
